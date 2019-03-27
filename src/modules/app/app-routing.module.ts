eb8830fc6573f5b72fbd84ccddf5001ea9b90c5b
SNMik <sychounm@gmail.com>
2019-03-27 17:37:19 +0300

    change auth module

---
+++ src/modules/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' } // по умолчанию редиректит на страницу с логином
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // регистрация корневых модулей
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
