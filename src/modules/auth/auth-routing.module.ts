22e212fba6ecf740e7fefe0f33b25884d6d1891d
SNMik <sychounm@gmail.com>
2019-04-03 10:08:32 +0300

    change modules import

--- src/modules/auth/auth-routing.module.ts
+++ src/modules/auth/auth-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthRoutingModule } from './auth-routing.module';
+
+import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { AuthComponent } from './auth/auth.component';
-import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     declarations: [
