169d4e131e4b82ad1131b2564221ad7279c92786
SNMik <sychounm@gmail.com>
2019-04-09 10:27:17 +0300

    save

--- src/modules/system/system-login/system-login.component.ts
+++ src/modules/system/system-login/system-login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from 'src/modules/shared/services/users.service';
+import { User } from 'src/modules/shared/models/user.model';
+
 @Component({
   selector: 'app-system-login',
   templateUrl: './system-login.component.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemLoginComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  authentificate: boolean;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.user = JSON.parse(window.localStorage.getItem('user'));
+    this.user ? this.authentificate = true : this.authentificate = false;
   }
 
 }
